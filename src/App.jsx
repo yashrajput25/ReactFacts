ce9170c9e8e3ce971926fe83ed1a1cb2f74b9350
Yash Rajput <yashrajputyr10@gmail.com>
2024-12-02 16:15:12 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar";
+import MainContent from "./components/Main";
+export default function App(){
+    return(
+        <div>
+             <NavBar />
+             <MainContent />
+        </div>
+       
+    );
+}
